79cfcbc51344026fc11b946577e17ce10627619f
anthonynarine <anarine83@gmail.com>
2023-06-29 11:48:57 -0400

    test written book instance creation; test passed

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,25 @@
 from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from books.models import Book
+
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = "Harry Potter and the Sorcerer's Stone",
+            subtitle = "introducing the world",
+            author = "J.K Rowling",
+            isbn = "9871735467221"
+        )
+    
+    def test_api_listview(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
 
 
-# Create your tests here.
+
